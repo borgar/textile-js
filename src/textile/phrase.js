07374c405eb06f07f8e121f9061bd0d89b30ea3c
Borgar <borgar@borgar.net>
2020-02-04 10:52:47 +0000

    Fixing an error in phrase bounds
    Closes #57

--- src/textile/phrase.js
+++ src/textile/phrase.js
@@ -101,7 +101,7 @@ function parsePhrase (src, options) {
         const t1 = re.escape(tok.charAt(0));
         mMid = (code) ? '^(\\S+|\\S+.*?\\S)'
           : `^([^\\s${t1}]+|[^\\s${t1}].*?\\S(${t1}*))`;
-        mEnd = '(?=$|[\\s.,"\'!?;:()«»„“”‚‘’])';
+        mEnd = '(?=$|[\\s.,"\'!?;:()«»„“”‚‘’<>])';
       }
       const rx = re.compile(`${mMid}(${re.escape(tok)})${mEnd}`);
       if ((m = rx.exec(src)) && m[1]) {
