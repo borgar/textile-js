e85a74a8ae94c5c3e1300c99c777ee8a69529cb6
GehDoc <GehDoc@users.noreply.github.com>
2020-02-27 09:12:45 +0100

    More linting

--- scripts/updatedingus.js
+++ scripts/updatedingus.js
@@ -1,5 +1,5 @@
 const fs = require( 'fs' );
-const updateMessage = "Updating textile lib to latest version.";
+const updateMessage = 'Updating textile lib to latest version.';
 
 function exec ( cmd, callback ) {
   require( 'child_process' ).exec( cmd, function ( err, stdout, stderr ) {
@@ -13,26 +13,18 @@ const src = fs.readFileSync( './lib/textile.min.js', 'utf8' );
 
 // switch to gh-pages branch
 exec( 'git checkout gh-pages', o => {
-
   // pull latest
   exec( 'git pull', o => {
-
     // write changes
     fs.writeFileSync( './textile.min.js', src, 'utf8' );
 
     // commit changes
     exec( `git commit -m "${ updateMessage }" textile.min.js`, o => {
-
       // push to github
-      exec( `git push`, o => {
-
+      exec( 'git push', o => {
         // switch back to master
-        exec( `git checkout master` );
-
+        exec( 'git checkout master' );
       });
-
     });
-
   });
-
 });
